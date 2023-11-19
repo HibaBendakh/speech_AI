eb4aad512dc62ba6e04a151b6570e863a6e167c3
HibaBendakhkhou <hiba.bendakhkhou@usmba.ac.ma>
2023-11-19 15:52:05 +0100

    commit 1

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,14 +1,18 @@
+import speech_recognition as sr 
+import pyttsx3 
+print("update")
+
 def record_txt():
    return 0
 
 def record_txt(txt):
    return 1
 
-while(1):
+while(0):
    txt = record_txt()
    record_txt(txt)
 
-print("wrote text")
+print("wrote text") 
 
 
 
